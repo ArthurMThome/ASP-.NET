6dc20a5626aa67254ab823dc648a9a09d6ee6b4b
Arthur Martins Thomé <40119529+ArthurMThome@users.noreply.github.com>
2020-08-14 19:54:55 -0300

    Criando projeto, começando a criar as paginas e tratando alguns erros.

---
+++ waAgenda/usuarios.aspx.cs
@@ -0,0 +1,63 @@
+﻿/// Arthur M. Thomé
+/// 14 AUG 2020
+/// Arthur Martins Thomé - Curso ASP .NET ( Udemy ).
+
+#region Statements
+
+using System;
+using System.Web;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections.Generic;
+
+#endregion
+
+namespace waAgenda
+{
+    public partial class usuarios : Page
+    {
+        #region Page Methods
+
+        protected void Page_Load ( object sender, EventArgs e )
+        {
+
+        }
+
+        #endregion
+
+        #region Events Data Base.
+
+        /// <summary>
+        /// Evento chamado após a ação de inserir ser realizada.
+        /// </summary>
+        /// <param name="sender">Próprio obj, no caso o DataSource.</param>
+        /// <param name="e">Events.</param>
+        protected void SqlDataSourceUsuarios_Inserted ( object sender, SqlDataSourceStatusEventArgs e )
+        {
+            CheckException ( e );
+        }
+
+        /// <summary>
+        /// Evento chamado após a ação de atualizar ser realizada.
+        /// </summary>
+        /// <param name="sender">Próprio obj, no caso o DataSource.</param>
+        /// <param name="e">Events.</param>
+        protected void SqlDataSourceUsuarios_Updated ( object sender, SqlDataSourceStatusEventArgs e )
+        {
+            CheckException ( e );
+        }
+
+        #endregion
+
+        private void CheckException ( SqlDataSourceStatusEventArgs e )
+        {
+            if ( e.Exception != null )
+            {
+                //lbMessage.Text = e.Exception.Message;
+                lbMessage.Text = "Campo 'email' ja existe ou existe algum campo em branco.";
+                e.ExceptionHandled = true;
+            }
+        }
+    }
+}
